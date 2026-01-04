6912de635824b9c6b199f9cd37cf5bf8ce019bbe
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2026-01-04 16:42:17 +0530

    understood how to use function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent ,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  name="venkat"
+  handleClickEvent(){
+    alert("function called")
+  }
+  otherFunction(){
+    console.log("other Function")
+  }
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+@Component({
+  selector:'app-profile',
+  //template:`<h1>Profile Component</h1>`,
+  //styles:'h1{}color:orange'
+  templateUrl:'./profile.component.html',
+  styleUrl:'./profile.component.css'
+
+})
+export class ProfileComponent{
+
+}
